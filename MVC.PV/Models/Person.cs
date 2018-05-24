2e946631062f8a85671aad09358c7367a6693cfc
gixLearning <38102077+gixLearning@users.noreply.github.com>
2018-05-24 10:42:24 +0200

    Initial commit

---
+++ MVC.PV/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC.PV.Models {
+
+    public class Person {
+        public string Name { get; set; }
+        public int Phonenumber { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ MVC.PV/Models/PersonViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.PV.Models {
+    public class PersonViewModel {
+        public IEnumerable<Person> People { get; set; }
+        public Person Person { get; set; }
+    }
+}
